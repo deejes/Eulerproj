82a5460f5f80c22c13d2f1cf7b203e3d3f707817
Devansh Jain <j.devansh29@gmail.com>
2017-09-16 23:37:41 -0700

    add working but 0(n^4) soln. needs improvement

--- p7.py
+++ p7.py
@@ -1,6 +1,7 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 
+print ('rtes')
 
 def is_prime(x):
     if x < 2:
@@ -11,6 +12,13 @@ def is_prime(x):
     return True
 #    l = [True for x in range(target)]
 #    print (l)
-for y in range(20):
-    if is_prime(y):
-        print (y)
+
+primes = []
+x = 2
+while len(primes) != 6:
+    if is_prime(x):
+        primes.append(x)
+    x +=1
+
+
+print (primes[-1])
