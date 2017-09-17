40a4551e6f2e62947d3e42adfd84e21fa0c5874a
Devansh Jain <j.devansh29@gmail.com>
2017-09-16 23:54:08 -0700

    add working solution

--- p7.py
+++ p7.py
@@ -1,12 +1,14 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 
-print ('rtes')
+
 
 def is_prime(x):
     if x < 2:
         return False
-    for y in range (2,x):
+    elif x == 2 or x == 3:
+        return True
+    for y in range (2,int(x**(1/2))+1):
         if x % y == 0:
             return False
     return True
@@ -15,10 +17,12 @@ def is_prime(x):
 
 primes = []
 x = 2
-while len(primes) != 6:
+while len(primes) !=10001:
     if is_prime(x):
         primes.append(x)
+        print (x,len(primes))
     x +=1
 
 
+#print (is_prime(6))
 print (primes[-1])
